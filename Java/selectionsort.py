93934c73a185b740ddda5614d7775c5b7c1aa48b
Thanuja-9014 <kurubathanuja15@gmail.com>
2025-09-21 21:01:36 +0530

    Add files via upload

---
+++ Java/selectionsort.py
@@ -0,0 +1,10 @@
+arr = [5, 2, 9, 1, 5]
+
+for i in range(len(arr)):
+    min_idx = i
+    for j in range(i+1, len(arr)):
+        if arr[j] < arr[min_idx]:
+            min_idx = j
+    arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+print("Sorted array:", arr)
